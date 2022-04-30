dd535ae5ae4aa06f841f43fb024aa924444db07e
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2022-04-30 20:58:02 -0300

    implementando a lógica de incremento do contador

--- diretivas-angular/src/app/app.component.ts
+++ diretivas-angular/src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ count=0;
  
-}
+ constructor(){}
+
+  ngOnInit() {
+   
+     let interval = setInterval(()=>{
+       
+      this.count++
+      
+       if(this.count===10){
+         clearInterval(interval)
+       }
+       
+      },1000
+      
+      )
+      
+    }
+     
+    }
+    
+  
+
